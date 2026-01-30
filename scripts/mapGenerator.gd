f72fdc48600806b80aa10739a708f137d7e4a164
Vesa Koski <vesa.koski@seepiagames.com>
2026-01-31 01:29:17 +0200

    update spritesheet

--- scripts/mapGenerator.gd
+++ scripts/mapGenerator.gd
@@ -5,8 +5,7 @@ const room_prefab: PackedScene = preload("res:///prefabs/room.tscn")
 
 const SIZE_X = 128
 const SIZE_Z = 128
-const map_cols = []
-const map_rows = []
+var map= []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,17 +18,21 @@ func _process(delta: float) -> void:
 	pass
 
 func loadLevel(level: int) -> void:
+	map = []
 	var json_as_text = FileAccess.get_file_as_string(level_0_Data)
 	var level_data = JSON.parse_string(json_as_text)
 	print(level_data)
 	for y in level_data.rooms.size():
+		var room_row = []
 		var rooms = level_data.rooms[y]
 		for x in rooms.size():
 			var room_info = rooms[x]
 			var room = room_prefab.instantiate()
 			room.transform.origin.x = x * SIZE_X
 			room.transform.origin.z = y * SIZE_Z
+			room_row.append(room)
 			if room_info == "#":
 				print("empty")
 			else:
 				print(room_info)
+		map.append(room_row)
